a63ee989b8340d10f013e8a553eaa65c21412510
Mohamed-Hedi-Gharbi <mohamedhedigharbi101@gmail.com>
2024-12-11 13:27:44 +0100

    thirst commit

--- src/utils.c
+++ src/utils.c
@@ -11,7 +11,7 @@
 //? Connexion global à PostgreSQL
 PGconn *conn = NULL;
 
-// Fonction pour charger les variables d'environnement depuis un fichier .env
+//TODO: Fonction pour charger les variables d'environnement depuis un fichier .env
 void load_env_file(const char *filename, char **db_name, char **db_user, char **db_password) {
     FILE *file = fopen(filename, "r");
     if (!file) {
@@ -21,7 +21,7 @@ void load_env_file(const char *filename, char **db_name, char **db_user, char **
 
     char line[256];
     while (fgets(line, sizeof(line), file)) {
-        line[strcspn(line, "\n")] = 0;  // Supprimer le caractère de nouvelle ligne
+        line[strcspn(line, "\n")] = 0;  //! Supprimer le caractère de nouvelle ligne
 
         if (strncmp(line, "DB_NAME=", 8) == 0) {
             *db_name = strdup(line + 8);
@@ -35,29 +35,29 @@ void load_env_file(const char *filename, char **db_name, char **db_user, char **
     fclose(file);
 }
 
-// Fonction pour se connecter à la base de données
+//TODO: Fonction pour se connecter à la base de données
 void connect_to_database() {
     char *db_name = NULL, *db_user = NULL, *db_password = NULL;
 
-    // Charger les variables d'environnement
+    //? Charger les variables d'environnement
     load_env_file(".env", &db_name, &db_user, &db_password);
 
-    // Construire la chaîne de connexion
+    //? Construire la chaîne de connexion
     char conninfo[256];
     snprintf(conninfo, sizeof(conninfo), "dbname=%s user=%s password=%s", db_name, db_user, db_password);
 
-    // Initialiser la connexion
+    //? Initialiser la connexion
     conn = PQconnectdb(conninfo);
 
     if (PQstatus(conn) != CONNECTION_OK) {
         fprintf(stderr, "Erreur de connexion à la base de données : %s\n", PQerrorMessage(conn));
         PQfinish(conn);
-        conn = NULL; // Marquer la connexion comme nulle
+        conn = NULL; //* Marquer la connexion comme nulle
     } else {
         printf("Connexion réussie à la base de données.\n");
     }
 
-    // Libérer les variables allouées dynamiquement
+    //? Libérer les variables allouées dynamiquement
     free(db_name);
     free(db_user);
     free(db_password);
